75ef358a6b3d3479135d626deb9da67362a38da8
Muskan Sagar <99795889+Muskan1283@users.noreply.github.com>
2022-04-06 17:04:32 +0530

    Add files via upload

---
+++ Muskan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 5;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = k; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+                k--;
+                    Console.WriteLine("     ");
+            }
+        }    
+    }
+}
